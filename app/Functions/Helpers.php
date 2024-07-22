38fe89e215c66b56bb9847eac12cf866191f9e38
lazzaric92 <163902177+lazzaric92@users.noreply.github.com>
2024-07-22 17:58:11 +0200

    Initial commit

---
+++ app/Functions/Helpers.php
@@ -0,0 +1,24 @@
+<?php
+
+class Helpers {
+    public static function getCsvInfo($filePath){
+        // > apro il file
+        $csvData = [];
+        $fileData = fopen($filePath, 'r');
+
+        // > se non trovi il file, dimmelo
+        if($fileData === false){
+            throw new InvalidArgumentException('File not found, check the file path');
+        };
+
+        // > se lo trovi, leggi riga per riga
+        while($csvRow = fgetcsv($fileData) !== false){
+            $csvData[] = $csvRow;
+        }
+
+        // > chiudo il file
+        fclose($fileData);
+
+        return $csvData;
+    }
+}
